a8e5f928772e38d6dddae031bd82d983fb73997b
au9ustine <duke.augustine@gmail.com>
2017-04-02 16:44:54 +0800

    add error handling of advanced

--- advanced/lib/advanced.ex
+++ advanced/lib/advanced.ex
@@ -6,4 +6,10 @@ defmodule Advanced do
       IO.puts "Result: #{result}"
     end
   end
+  defmodule ExampleError do
+    defexception message: "an example error has occurred"
+  end
+  def my_throw() do
+    raise(ArgumentError, message: "you have a typo")
+  end
 end
--- advanced/test/advanced_test.exs
+++ advanced/test/advanced_test.exs
@@ -7,4 +7,11 @@ defmodule AdvancedTest do
     alias Advanced.Example
     assert capture_io(fn -> Example.timed(fn (n) -> (n * n) * n end, [100]) end) =~ ~r/Result: 1000000/i
   end
+
+  test "error handling" do
+    import Advanced
+    assert_raise ArgumentError, fn -> my_throw() end
+    # `assert_raise ArgumentError, my_throw` would fail. See
+    # http://stackoverflow.com/questions/37703632/elixir-assert-raise-doesnt-catch-exceptions
+  end
 end
