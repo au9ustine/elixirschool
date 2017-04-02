40d3efe5b333353af8f46c92f9c0f91fd73a8de8
au9ustine <duke.augustine@gmail.com>
2017-04-02 17:22:48 +0800

    add concurrency of advanced

--- advanced/lib/advanced.ex
+++ advanced/lib/advanced.ex
@@ -5,6 +5,46 @@ defmodule Advanced do
       IO.puts "Time: #{time}ms"
       IO.puts "Result: #{result}"
     end
+
+    # Concurrency - Processes
+    def add(a, b) do
+      IO.puts(a + b)
+    end
+
+    # Concurrency - Message Passing
+    def listen do
+      receive do
+        {:ok, "hello"} -> IO.puts "World"
+      end
+
+      listen
+    end
+    def explode, do: exit(:kaboom)
+
+    # Concurrency - Process Linking
+    # def run do
+    #   Process.flag(:trap_exit, true)
+    #   spawn_link(Example, :explode, [])
+
+    #   receive do
+    #     {:EXIT, from_pid, reason} -> IO.puts "Exit reason: #{reason}"
+    #   end
+    # end
+
+    # Concurrency - Process Monitoring
+    # def run do
+    #   {pid, ref} = spawn_monitor(Example, :explode, [])
+
+    #   receive do
+    #     {:DOWN, ref, :process, from_pid, reason} -> IO.puts "Exit reason: #{reason}"
+    #   end
+    # end
+
+    # Concurrency - Tasks
+    def double(x) do
+      :timer.sleep(2000)
+      x * 2
+    end
   end
   defmodule ExampleError do
     defexception message: "an example error has occurred"
--- advanced/test/advanced_test.exs
+++ advanced/test/advanced_test.exs
@@ -14,4 +14,16 @@ defmodule AdvancedTest do
     # `assert_raise ArgumentError, my_throw` would fail. See
     # http://stackoverflow.com/questions/37703632/elixir-assert-raise-doesnt-catch-exceptions
   end
+
+  test "concurrency" do
+    {:ok, agent} = Agent.start_link(fn -> [1, 2, 3] end)
+    Agent.update(agent, fn (state) -> state ++ [4, 5] end)
+    assert Agent.get(agent, &(&1)) == [1,2,3,4,5]
+    Agent.start_link(fn -> [1, 2, 3] end, name: Numbers)
+    assert Agent.get(Numbers, &(&1)) == [1,2,3]
+    alias Advanced.Example
+    task = Task.async(Example, :double, [2000])
+    IO.puts "Do some other works but the task is still in-progress"
+    assert Task.await(task) == 4000
+  end
 end
